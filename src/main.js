ed121402006e77ab08007c6efd742ecc354fc623
Katherine Ramirez <oeillet8_5@hotmail.com>
2019-07-01 14:58:23 -0400

    hola

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 /* Manejo del DOM */
+const name = "Alicia";
+const lastName = "Díaz";
+
+const containerDatos = document.getElementById('container_datos');
+document.getElementById('clickme').addEventListener('click', () => {
+  containerDatos.innerHTML = `<h2 class="text_datos"> 
+                                Tu nombre es: ${name}
+                              </h2>
+                              <h2 class="text_datos"> 
+                                Tu Apellido es: ${lastName}
+                              </h2>
+                              <img src="./9BE.jpg" alt="planta del pie" class="photo">`
+})
+
+const datos = STUDENTS.student;
+for(let i = 0; i < datos.length; i++){
+  console.log(datos[i].name);
+  console.log(datos[i].lastName);
+  console.log(datos[i].hobby);
+}
