6782f99c3dfa240e92f69d144dc3db738609f83f
Claudia  Heredia <claudiaheredia@MacBook-de-Claudia.local>
2019-07-10 15:09:52 -0400

    Mostrando Data

--- src/data.js
+++ src/data.js
@@ -1,39 +1,16 @@
-/* Manejo de data 
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
-  return 'example';
-};
-
-window.example = example; */
-
-const datos = POKEMON.pokemon;
+/*const datos = POKEMON.pokemon;
 for(let i = 0; i < datos.length; i++)
 {
     console.log(datos[i]); 
 };
+*//////////////////////////////////////////
 
-const filterDatos = datos.filter(element =>(
-  console.log(element.type)
-)); 
+
+
+const example = () => {
+	return 'example';
+  };
   
+  window.example = example;
 
-/*  console.log(datos[i].id);
-    console.log(datos[i].num);
-    console.log(datos[i].name);
-    console.log(datos[i].img);
-    console.log(datos[i].type);
-    console.log(datos[i].height);
-    console.log(datos[i].weight);
-    console.log(datos[i].candy);
-    console.log(datos[i].candy_count);
-    console.log(datos[i].egg);
-    console.log(datos[i].spawn_chance);
-    console.log(datos[i].avg_spawns);
-    console.log(datos[i].spawns_time);
-    console.log(datos[i].multiplers);
-    console.log(datos[i].weaknesses);
-    console.log(datos[i].next_evolution);
-*/
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
-/* Manejo del DOM */
-const selectName = document.getElementById("filterType");
-//console.log(selectName); 
-selectName.addEventListener('change', ()=> {
-    let condition = selectName.options[selectName.selectedIndex].value;
-    console.log(condition)
-})
+// AQUI CREO UNA COSNTANTE QUE ME MUESTRE LA DATA.
+const showData = window.POKEMON.pokemon;
+	
+         for ( let i=0; i < showData.length; i++) {
 
+			let result = document.createElement("div");
+			result.id = "result";
+			
+			let name= document.createElement("h3");
+			name.textContent = showData[i].name; 
+
+			let egg= document.createElement("p");
+			egg.textContent = showData[i].egg; 
+
+			//let type= documente.createElement("p");
+			//type.textContent =  showData[i].type; 
+        
+			let imagen = document.createElement("img");
+			imagen.src = showData[i].img;
+			
+			result.appendChild(name);
+			result.appendChild(egg); 
+			//result.appendChild(type); 
+			result.appendChild(imagen);
+			document.getElementById("cards").appendChild(result).innerHTML; 	
+		
+
+		console.log(result);
+	}
+	
+/*	var array1 = [];
+
+array1.forEach(function(element) {
+  console.log(element);
+});
+
+*/
